b1a9583932e3a2649a404b89084d0499ee149e64
Olga-Gromova <gromova.o.77@gmail.com>
2023-12-24 13:44:01 +0100

    for checking

--- models/user.js
+++ models/user.js
@@ -39,8 +39,6 @@ const userSchema = new Schema(
 	{ versionKey: false, timestamps: true }
 );
 
-// const emailRegex = /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/;	
-
 const loginSchema = Joi.object({
 	email: Joi.string().pattern(emailRegex).required(),
 	password: Joi.string().min(6).required(),
